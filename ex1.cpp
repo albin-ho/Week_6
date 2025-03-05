d6f4a16f7419688aae3b9020a98436621b82a87f
Albin Ho <albin_ho@yahoo.co.uk>
2025-03-05 11:44:53 +0000

    started file

---
+++ ex1.cpp
@@ -0,0 +1,17 @@
+// Program Purpose:No purpose yet
+// Author:
+// Last Modified:06/03/2025
+//
+//**********************************************************************************
+// #include <cmath>
+// #include <cstdlib>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <iomanip>
+// #include <ctime>
+// #include <fstream>
+// #include <iostream>
+using namespace std;
+
+int main() { return 0; }
